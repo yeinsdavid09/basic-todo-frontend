caed88cb9c6da3ec9aa8a7d025691bdc1c85aead
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2024-10-13 12:42:55 -0500

    fix(general)/scrollbar styles,components get props

--- src/app/layout/Layout.js
+++ src/app/layout/Layout.js
@@ -10,6 +10,14 @@ import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 import { Button } from "../components/Button/Button";
 
+const defaultTasks = [
+  { id: 1, description: "Task 1", completed: true },
+  { id: 2, description: "Task 2", completed: false },
+  { id: 3, description: "Task 3", completed: false },
+  { id: 4, description: "Task 4", completed: true },
+  { id: 5, description: "Task 5", completed: false },
+];
+
 export function Layout() {
   return (
     <div className="LAYOUT__main-container">
@@ -25,9 +33,16 @@ export function Layout() {
         </div>
         <div className="LAYOUT__list">
           <List>
-            <Item></Item>
-            <Item></Item>
-            <Item></Item>
+            {defaultTasks.map((task, index) => {
+              return (
+                <Item
+                  key={task.id}
+                  index={index + 1}
+                  description={task.description}
+                  completed={task.completed}
+                ></Item>
+              );
+            })}
           </List>
         </div>
         <div className="LAYOUT__buttons">
