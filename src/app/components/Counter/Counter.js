83c1481f715dcf216b55517a62605befcba47cab
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2024-10-13 12:41:50 -0500

    fix(counter)/counter gets props

--- src/app/components/Counter/Counter.js
+++ src/app/components/Counter/Counter.js
@@ -1,11 +1,11 @@
 //* Base
 import "./Counter.css";
 
-export function Counter() {
+export function Counter({ completed = 0, total = 0 }) {
   return (
     <div className="COUNTER__main-container">
       <h2 className="COUNTER__title GLOBAL__text-heading--3">
-        Has completado 2 de 5 tareas
+        Has completado {completed} de {total} tareas
       </h2>
     </div>
   );
