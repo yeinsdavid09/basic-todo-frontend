104f00239cfd88dd2ca305a5b68c747357944551
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2024-10-13 12:42:23 -0500

    fix(item)/item gets props, stykes improve

--- src/app/components/Item/Item.js
+++ src/app/components/Item/Item.js
@@ -1,12 +1,19 @@
 //* Base
 import "./Item.css";
 
-export function Item() {
+export function Item({ index, description, completed }) {
   return (
-    <li className="ITEM__container">
-      <span className="ITEM__check">v</span>
-      <span className="ITEM__text GLOBAL__text-body--1">Hola</span>
-      <span className="ITEM__remove">x</span>
+    <li className={`ITEM__container ${completed && "--completed"}`}>
+      <div className="ITEM__texts--wrapper">
+        <span className="ITEM__button --index GLOBAL__text-body--1">
+          {index}
+        </span>
+        <span className="ITEM__text GLOBAL__text-body--1">{description}</span>
+      </div>
+      <div className="ITEM__buttons--wrapper">
+        <span className="ITEM__button --check">v</span>
+        <span className="ITEM__button --remove">x</span>
+      </div>
     </li>
   );
 }
