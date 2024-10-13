d9f98a47c3e25fc0f5a3b0485f9470e24becd08c
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2024-10-13 00:07:29 -0500

    feat(counter)/counter layout and styles

---
+++ src/app/components/Counter/Counter.js
@@ -0,0 +1,12 @@
+//* Base
+import "./Counter.css";
+
+export function Counter() {
+  return (
+    <div className="COUNTER__main-container">
+      <h2 className="COUNTER__title GLOBAL__text-heading--3">
+        Has completado 2 de 5 tareas
+      </h2>
+    </div>
+  );
+}
