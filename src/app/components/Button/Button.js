82bad9c16143b5090312db2edcad4fb519c9f682
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2024-10-13 11:34:54 -0500

    feat(button)/button initial styles and layout

---
+++ src/app/components/Button/Button.js
@@ -0,0 +1,10 @@
+//* Base
+import "./Button.css";
+
+export function Button() {
+  return (
+    <button className="BUTTON__main-container">
+      <span className="BUTTON__span">+</span>
+    </button>
+  );
+}
