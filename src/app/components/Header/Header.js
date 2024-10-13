abb1a170eb7b1dfb9149f6900aea6dc3eb6dca5f
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2024-10-13 12:41:34 -0500

    fix(header)/typo

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 export function Header() {
   return (
     <div className="HEADER__main-container">
-      <h1 className="HEADER__title GLOBAL__text-heading--1">Basic Too App</h1>
+      <h1 className="HEADER__title GLOBAL__text-heading--1">Basic Todo App</h1>
     </div>
   );
 }
